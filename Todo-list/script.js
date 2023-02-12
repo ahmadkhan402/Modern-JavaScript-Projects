91fe3cba0d7248261c8f44be1e6b380e14cb18d3
Ahmad Saffiullah khan khattak <ahmadsafiullah777gmail.com>
2023-02-12 21:36:15 +0500

    Updated

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -3,6 +3,33 @@ const input = document.querySelector("input")
 const list = document.querySelector("ul")
 form.addEventListener("submit" , function(event){
     event.preventDefault();
+    Todo();
     input.value = ""
 })
+function Todo(){
+    const value = input.value;
+    if(value==="")  {
+        alert("Type Something")
+    }
+    else {
+const newlist = document.createElement("li")
+newlist.textContent = value;
+list.append(newlist)
+
+const Dltbutton = document.createElement('BUTTON');
+Dltbutton.textContent =  " Delete";
+newlist.append("",Dltbutton)
+
+
+    }
+}
+list.addEventListener("click", function(event){
+    if(event.target.nodeName === "BUTTON"){
+       event.target.closest('li').remove();
+       
+    }
+   
+   })
+
+
 
